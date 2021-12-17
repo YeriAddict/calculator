202251579423a93d7458233e1c14785ec387b62d
LEANG Denis <denis.leang@gmail.com>
2021-12-17 01:58:43 +0100

    Added javadoc to ScientificPanel class

--- src/main/java/leang/corporation/panel/ScientificPanel.java
+++ src/main/java/leang/corporation/panel/ScientificPanel.java
@@ -1,9 +1,21 @@
 package leang.corporation.panel;
 
+/**
+ * Class ScientificPanel is a singleton inheriting all methods from class Panel.
+ * 
+ * @see Panel
+ * @author LEANG Denis
+ */
 public class ScientificPanel extends Panel {
 
+	/**
+	 * ScientificPanel object
+	 */
 	private static ScientificPanel uniqueInstance = null;
-	
+
+	/**
+	 * Creates a Panel with all the properties in "Scientific Mode"
+	 */
 	public ScientificPanel() {
 		this.setTextField();
 		this.setProperties();
@@ -11,7 +23,12 @@ public class ScientificPanel extends Panel {
 		this.addComponentsToPanel("Scientific Mode");
 		this.addActionEvent("Scientific Mode");
 	}
-	
+
+	/**
+	 * Handles singleton design pattern
+	 * 
+	 * @return Unique ScientificPanel object
+	 */
 	public static ScientificPanel getInstance() {
 		if (uniqueInstance == null) {
 			uniqueInstance = new ScientificPanel();
