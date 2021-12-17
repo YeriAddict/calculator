ae123c545c58fd0d5e1f65b79dcf4f8f05c08364
LEANG Denis <denis.leang@gmail.com>
2021-12-17 01:16:48 +0100

    Added javadoc to MainFrame class

--- src/main/java/leang/corporation/frame/MainFrame.java
+++ src/main/java/leang/corporation/frame/MainFrame.java
@@ -5,6 +5,16 @@ import javax.swing.*;
 import leang.corporation.panel.StandardPanel;
 import leang.corporation.panel.ScientificPanel;
 
+/**
+ * Class MainFrame contains methods for the main frame. For instance, a
+ * JTabbedPane is added to the frame with various properties. This JTabbedPane contains StandardPanel and ScientificPanel.
+ * 
+ * @see MainFrame
+ * @see Panel
+ * @see StandardPanel
+ * @see ScientificPanel
+ * @author LEANG Denis
+ */
 public class MainFrame extends JFrame {
 
 	FrameManager manager;
@@ -12,6 +22,11 @@ public class MainFrame extends JFrame {
 	StandardPanel standardPanel = StandardPanel.getInstance();
 	ScientificPanel scientificPanel = ScientificPanel.getInstance();
 
+	/**
+	 * Creates a MainFrame object
+	 * 
+	 * @see FrameManager
+	 */
 	public MainFrame(FrameManager manager) {
 		this.manager = manager;
 		addComponentsToTabbedPane();
@@ -19,11 +34,26 @@ public class MainFrame extends JFrame {
 		this.add(modeTab);
 	}
 
+	/**
+	 * Adds components such as Panel objects to the JTabbedPane
+	 * 
+	 * @see StandardPanel
+	 * @see ScientificPanel
+	 */
 	public void addComponentsToTabbedPane() {
 		modeTab.add("Standard Mode", standardPanel);
 		modeTab.add("Scientific Mode", scientificPanel);
 	}
 
+	/**
+	 * Adds properties to the JTabbedPane
+	 * 
+	 * Those properties are :
+	 * <ul>
+	 * <li>Width and Height of the JButton
+	 * <li>Font and Font size
+	 * </ul>
+	 */
 	public void setTabbedPaneProperties() {
 		modeTab.setSize(350, 550);
 		modeTab.setFont(new java.awt.Font("Lato", Font.BOLD, 12));
